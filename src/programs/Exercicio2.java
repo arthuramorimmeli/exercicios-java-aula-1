6418f84ae22696fcb9b07d2fff3b139175f733f9
arthuramorimmeli <96141013+arthuramorimmeli@users.noreply.github.com>
2021-12-16 19:42:33 -0300

    adicioado exercicio 2

---
+++ src/programs/Exercicio2.java
@@ -0,0 +1,45 @@
+package programs;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Exercicio2 {
+
+    private Integer quantityNumbers;
+    private Integer multipleOf;
+    private List<Integer> multipleList;
+
+    public void runProgram() {
+        this.initValues();
+        this.getUserInputValues();
+        this.multipleList = this.generateMultipleList(this.multipleOf, quantityNumbers);
+        Exercicio1.showNumsEven(this.multipleList);
+    }
+
+    private void initValues() {
+        this.quantityNumbers = 0;
+        this.multipleOf = 0;
+        this.multipleList = new ArrayList<>();
+    }
+
+    private void getUserInputValues() {
+        this.quantityNumbers = this.sendMessageAndgetIntegerValue("Quantos numeros você precisa?");
+        this.multipleOf = this.sendMessageAndgetIntegerValue("Eles devem ser multiplos de: ");
+    }
+
+    private List<Integer> generateMultipleList(Integer multiple, Integer sizeList) {
+        ArrayList<Integer> multipleList = new ArrayList<>();
+        for (int i = 0;  sizeList != multipleList.size();  i++) {
+            if ((i%multiple) == 0) multipleList.add(i);
+        }
+        return multipleList;
+    }
+
+    private Integer sendMessageAndgetIntegerValue(String message) {
+        System.out.println(message);
+        Scanner sc = new Scanner(System.in);
+        return sc.nextInt();
+    }
+
+}
